92e461efd5619d56ed25eb78829aaedac4f742b3
Farhod <shermuhammadfarhod@gmail.com>
2023-08-02 12:32:07 +0500

    adding views

--- human/views.py
+++ human/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from .models import Human
+from django.http import JsonResponse
 # Create your views here.
+
+def get_all(request):
+    if request.method=="GET":
+        all_date= Human.objects.all()
+        result=[]
+        for human in all_date:
+             result.append({"id":human.id,"human_name":human.first_name})
+        return JsonResponse({"date":result})
+
+def get_by_id(request,human_id):
+    if request.method=="GET":
+        try:
+            date=Human.objects.get(id=human_id)
+        except Human.DoesNotExists:
+            return JsonResponse({"msg": "NOT FOUND"})
+        return JsonResponse({"id":date.id,"pupil_name":date.first_name})
